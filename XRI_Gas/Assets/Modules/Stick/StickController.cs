e5267ea82c544511831823b00dc629ea4242998b
Max <LegendBlake@mail.ru>
2026-02-25 11:17:32 +0500

    v31 (Refactoring)

--- XRI_Gas/Assets/Modules/Stick/StickController.cs
+++ XRI_Gas/Assets/Modules/Stick/StickController.cs
@@ -1,85 +1,123 @@
-﻿using UnityEngine;
+﻿// НАЗНАЧЕНИЕ: Управление логикой зажигания спички при взаимодействии с коробком в VR
+
+using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using NaughtyAttributes;
+using Cysharp.Threading.Tasks;
 
 public class StickController : MonoBehaviour
 {
-    [Header("Настройки огня")]
-    public GameObject flameVisuals;
-    public bool isLit = false;
+    #region Поля: Required
+    [BoxGroup("Required"), Required, SerializeField]
+    private GameObject _flameVisuals;
 
-    [Header("Настройки чирканья")]
-    public float ignitionThreshold = 0.5f;
-    public string matchboxTag = "Matchbox";
+    [BoxGroup("Required"), Required, SerializeField]
+    private AudioSource _ignitionSound;
 
-    [Header("Звук")]
-    [Tooltip("Перетащите сюда AudioSource со звуком зажигания")]
-    public AudioSource ignitionSound;
+    [BoxGroup("Required"), Required, SerializeField]
+    private XRGrabInteractable _grabInteractable;
+    #endregion
 
-    [SerializeField] private XRGrabInteractable grabInteractable;
-    private Vector3 lastPosition;
-    private Vector3 currentVelocity;
+    #region Поля
+    [BoxGroup("SETTINGS"), SerializeField]
+    private float _ignitionThreshold = 0.5f;
 
+    [BoxGroup("SETTINGS"), SerializeField]
+    private string _matchboxTag = "Matchbox";
+
+    [BoxGroup("DEBUG"), SerializeField, ReadOnly]
+    private bool _isLit;
+
+    [BoxGroup("DEBUG"), SerializeField]
+    private bool _coloredDebug;
+
+    [BoxGroup("DEBUG"), SerializeField, ReadOnly]
+    private Vector3 _currentVelocity;
+    #endregion
+
+    #region Свойства
+    public bool IsLit => _isLit;
+    #endregion
+
+    #region Unity Методы
     private void Awake()
     {
-        if (flameVisuals != null) flameVisuals.SetActive(false);
+        if (_flameVisuals == null) DebugUtils.LogMissingReference(this, nameof(_flameVisuals));
+        if (_ignitionSound == null) DebugUtils.LogMissingReference(this, nameof(_ignitionSound));
+        if (_grabInteractable == null) DebugUtils.LogMissingReference(this, nameof(_grabInteractable));
+
+        if (_flameVisuals != null)
+            _flameVisuals.SetActive(false);
     }
 
+    private void OnEnable() => _grabInteractable.selectExited.AddListener(OnDropped);
+    private void OnDisable() => _grabInteractable.selectExited.RemoveListener(OnDropped);
+
+    private Vector3 _lastPosition;
     private void Update()
     {
-        currentVelocity = (transform.position - lastPosition) / Time.deltaTime;
-        lastPosition = transform.position;
+        _currentVelocity = (transform.position - _lastPosition) / Time.deltaTime;
+        _lastPosition = transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isLit) return;
+        if (_isLit) return;
 
-        if (other.CompareTag(matchboxTag))
+        if (other.CompareTag(_matchboxTag))
         {
-            Vector3 boxVelocity = Vector3.zero;
-            Rigidbody boxRb = other.attachedRigidbody;
+            float speed = _currentVelocity.magnitude;
 
-            float speed = currentVelocity.magnitude;
+            ColoredDebug.CLog(gameObject, "<color=cyan>[INFO]</color> Попытка зажигания. Скорость: {0}", _coloredDebug, speed);
 
-            Debug.Log($"Чирканье (триггер)! Скорость: {speed}");
-
-            if (speed >= ignitionThreshold)
-            {
-                Ignite();
-            }
+            TryIgnite(speed);
         }
     }
+    #endregion
 
-    private void Ignite()
+    #region Личные методы
+    private bool TryIgnite(float speed)
     {
-        isLit = true;
-        if (flameVisuals != null) flameVisuals.SetActive(true);
-       
-        if (ignitionSound != null)
-        {
-            ignitionSound.Play();
-        }
+        if (speed < _ignitionThreshold) return false;
+
+        ApplyIgnition();
+
+        ColoredDebug.CLog(gameObject, "<color=lime>[ACTION]</color> Объект успешно зажжен", _coloredDebug);
+
+        return true;
+    }
+
+    private void ApplyIgnition()
+    {
+        _isLit = true;
+
+        if (_flameVisuals != null)
+            _flameVisuals.SetActive(true);
+
+        if (_ignitionSound != null)
+            _ignitionSound.Play();
 
         SendHaptic(0.7f, 0.15f);
     }
 
-    private void OnEnable() => grabInteractable.selectExited.AddListener(OnDropped);
-    private void OnDisable() => grabInteractable.selectExited.RemoveListener(OnDropped);
-
     private void OnDropped(SelectExitEventArgs args)
     {
-        isLit = false;
-        if (flameVisuals != null) flameVisuals.SetActive(false);
+        _isLit = false;
+        if (_flameVisuals != null)
+            _flameVisuals.SetActive(false);
+
+        ColoredDebug.CLog(gameObject, "<color=orange>[SYSTEM]</color> Объект брошен, огонь потушен", _coloredDebug);
     }
 
     private void SendHaptic(float intensity, float duration)
     {
-        if (grabInteractable.interactorsSelecting.Count > 0)
+        if (_grabInteractable.interactorsSelecting.Count > 0)
         {
-            var interactor = grabInteractable.interactorsSelecting[0];
+            var interactor = _grabInteractable.interactorsSelecting[0];
             if (interactor is UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInputInteractor inputInteractor)
                 inputInteractor.SendHapticImpulse(intensity, duration);
         }
     }
+    #endregion
 }
